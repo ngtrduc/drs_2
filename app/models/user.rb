bb24cd0346b63d112d6f335864436ac17358aad6
daonhat <daonhat123@gmail.com>
2016-07-05 12:56:15 +0700

    refs create 67665 relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,16 @@ class User < ActiveRecord::Base
     foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :following, through: :active_relationships, source: :followed
+
+  def follow other_user
+    active_relationships.create followed_id: other_user.id
+  end
+
+  def unfollow other_user
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
+  end
 end
